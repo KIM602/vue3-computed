1b3c9bd479a2743da94a9495860b2853de5a7e46
KIM602 <kyk940608@gmail.com>
2023-12-18 09:44:58 +0900

    Computed 캐싱

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
-    <Fruits />
+    <h1>{{ reversedMessage }}</h1>
+    <h1>{{ reversedMessage }}</h1>
+    <h1>{{ reversedMessage }}</h1>
+    <h1>{{ reversedMessage }}</h1>
 </template>
 
 <script>
-import Fruits from '~/components/Fruits'
-
 export default {
-    components: {
-        Fruits
+    data() {
+        return {
+            msg: 'Hello Computed!'
+        }
+    },
+    computed: { // 메소드처럼 동작하지만 캐싱기능이 있음
+        reversedMessage() {
+            return this.msg.split('').reverse().join('')
+        }
     }
-
 }
 </script>
